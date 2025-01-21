87ba3007ef0bb67bb7ad61fcb4f3b2d79dfaddd8
echo66677 <120939302+echo66677@users.noreply.github.com>
2025-01-21 17:53:38 +0800

    Update xiaozujianhezi.js

--- iTunes/xiaozujianhezi.js
+++ iTunes/xiaozujianhezi.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes/xiaozujianhezi.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/echo66677/Rewrite/refs/heads/main/iTunes/xiaozujianhezi.js
 
 [mitm]
 hostname = buy.itunes.apple.com
