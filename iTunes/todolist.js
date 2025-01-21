d563e3a21f650daa3c78aa9fbebcbd8d2c37a29b
echo66677 <120939302+echo66677@users.noreply.github.com>
2025-01-21 18:09:52 +0800

    Update todolist.js

--- iTunes/todolist.js
+++ iTunes/todolist.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes/todolist.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/echo66677/Rewrite/refs/heads/main/iTunes/todolist.js
 
 [mitm]
 hostname = buy.itunes.apple.com
